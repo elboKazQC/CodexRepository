5d51d625f3c8cd96723ffeff0fcdf6963fd27dc5
elboKazQC <casaubonvincent@gmail.com>
2025-05-21 16:55:11 -0400

    retour en arriere

--- AuditWifiApp/src/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>'],
-  testMatch: ['<rootDir>/tests/**/*.test.ts'],
-  moduleNameMapper: {
-    '^vscode$': '<rootDir>/tests/__mocks__/vscode.ts'
-  }
-};
